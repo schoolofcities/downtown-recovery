0ad44ea3f5e4e579e96ef918ad07aebdfad798e7
Hannah Moore <hmoore@berkeley.edu>
2022-07-21 15:15:39 -0700

    updates to index

--- shinyapp/ranking_widget.R
+++ shinyapp/ranking_widget.R
@@ -4,5 +4,4 @@ source("dtra_functions.R")
 
 plot_obj <- recovery_rankings_plot(recovery_rankings_df("downtown"))
 
-
 htmlwidgets::saveWidget(plot_obj, "../docs/widgets/recovery_rankings.html", selfcontained = FALSE)
