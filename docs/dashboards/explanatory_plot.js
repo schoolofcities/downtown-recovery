1cb64ad04ab4652893010ddf721644a55b2d6fd9
Hannah Moore <hmoore@berkeley.edu>
2022-07-23 14:35:56 -0700

    add 3 metric df

--- docs/dashboards/explanatory_plot.js
+++ docs/dashboards/explanatory_plot.js
@@ -1,48 +1,57 @@
+
+
 // load in data
-const unfilteredData = d3.csv("../model_data_long.csv").then(function(data){
-    var x_vals = [], y_vals = [], x_vars = []; cities = []; regions = []; metrics = [];
+Plotly.d3.csv('https://raw.githubusercontent.com/hmooreo/downtownrecovery/main/docs/model_data.csv', function(err, rows){
+
+  function unpack(rows, key) {
+    return rows.map(function(row) { return row[key]; });
+}
+
+    /*var x_vals = [], y_vals = [], x_vars = []; cities = []; regions = []; metrics = [];
   
-    for (var i = 0; i < allRows.length; i++) {
+        for (var i = 0; i < unfilteredData.length; i++) {
   
-      row = allRows[i];
+            row = unfilteredData[i];
   
-      x_vals.push(row['x_val']);
-      x_vars.push(row['x_var']);
-      cities.push(row['display_title']);
-      regions.push(row['region']);
-      metrics.push(row['metric']);
-      y_vals.push(row['y']);
+            x_vals.push(row['x_val']);
+            x_vars.push(row['x_var']);
+            cities.push(row['display_title']);
+            regions.push(row['region']);
+            metrics.push(row['metric']);
+            y_vals.push(row['y']);
   
+            }
+
+        return({x : x_vals, variable : x_vars, y : y_vals, city : cities, region : regions, metric : metrics});*/
+
+    var plotDiv = document.getElementById('explanatory_plot');
+
+    var e_x = document.getElementById('x_vars');
+
+    var x_variable = e_x.options[e_x.selectedIndex].value;
+
+    var e_m = document.getElementsByName('metric');
+
+    for (var i = 0; i < e_m.length; i++) {
+        var this_metric = e_m[i];
+        if (this_metric.checked) {
+            var selected_metric = this_metric.value;
+        }
     }
-
-    return({x : x_vals, variable : x_vars, y : y_vals, city : cities, region : regions, metric : metrics});
-});
-
-function makePlotly(){
-  
-    var plotDiv = document.getElementById("explanatory_plot");
-
-    var x_variable = document.getElementById("x_vars").value;
-
-    var selected_metric = document.getElementById("select_metric").value;
-
-    const plot_data = unfilteredData.filter(element => {
-        return element.metric === selected_metric && element.x_var === x_variable;
-    });
 
     var trace = [{
-  
-        x: plot_data['x'],
-        y: plot_data['y'],
-        mode: 'markers',
-        type: 'scatter',
+        type: "scatter",
+        mode: "markers",
+        x: unpack(rows, x_variable),
+        y: unpack(rows, 'y'),
+        metric: unpack(rows, 'metric'),
+        city: unpack(rows, 'city'),
+        region: unpack(rows, 'region')
       }];
-    
-    
-      Plotly.newPlot(plotDiv, trace,
-    
-        {title: 'Plotting CSV data from AJAX call'});
 
-    }
-
-makePlotly();
+    const plot_data = Object.entries(trace).filter(currentElement => {
+        return(currentElement.metric === selected_metric);
+    });
+    
+      Plotly.newPlot(plotDiv, plot_data, {title: 'Plotting CSV data from AJAX call'});
+})
