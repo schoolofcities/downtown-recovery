e2cddc89bea899c58736dde15d36daf0ed8698af
Jeff Allen <jeffallen89@gmail.com>
2023-01-10 10:25:08 -0500

    change colours ranking plot

--- docs/dashboards/rankings_plot.js
+++ docs/dashboards/rankings_plot.js
@@ -1,7 +1,7 @@
 
 
 // load in data
-Plotly.d3.csv('https://raw.githubusercontent.com/schoolofcities/downtown-recovery/cuebiq_updates/docs/ranking_data_cuebiq_update.csv', function (err, rows) {
+Plotly.d3.csv('https://raw.githubusercontent.com/schoolofcities/downtown-recovery/main/docs/ranking_data_cuebiq_update.csv', function (err, rows) {
 
     function unpack(rows, key) {
         return rows.map(function (row) { return row[key]; });
