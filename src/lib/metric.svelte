a3ae614f74b7b6ad89cad99c915fd3ed650185d3
unknown <hmoore@berkeley.edu>
2023-05-31 12:53:58 -0700

    begin adding components to lib

---
+++ src/lib/metric.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Select from "../lib/Select.svelte";
+    let selected;
+</script>
+    
+    <Select options={[{"text":"aaa"},{"text":"bbb"},{"text":"ccc"}]}
+                    bind:value={selected}/>
+    {JSON.stringify(selected, null, 2)}
+    {(console.log(selected),'')}
