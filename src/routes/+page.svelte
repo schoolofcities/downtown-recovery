f78f655451b3621f0a7b315d397b4b90f6c2f9de
Jeff Allen <jeffallen89@gmail.com>
2023-06-15 16:39:54 -0600

    start addin content to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,4 +8,64 @@
 
 <Header/>
 
+<main>
 
+    <div class="text">
+
+        <h1>
+            Death of Downtown?
+        </h1>
+        <p>
+            <span class="bold">Exploring Pandemic Recovery Trajectories Across 63 North American Cities</span>
+        </p>
+        <p>
+            Updated on April 25, 2023
+        </p>
+
+    </div>
+
+    <div id="cards-wrapper">
+
+        <div class="card">
+
+            <h2>Recovery Rankings</h2>
+
+            <p>Comparison of overall recovery in activity levels across different cities and time periods.</p>
+
+        </div>
+
+        <div class="card">
+
+            <h2>Recovery Patterns</h2>
+
+            <p>Explore weekly activity levels during different phases of the pandemic for different cities.</p>
+
+        </div>
+
+        <div class="card">
+
+            <h2>Policy Brief</h2>
+
+            <p>Our report (updated January 2023) on recovery patterns and their explanatory factors.</p>
+
+        </div>
+
+        <div class="card">
+
+            <h2>Explainer Video</h2>
+
+            <p>Our short video on downtown recovery on what needs to be done to prevent the death of downtown.</p>
+
+        </div>
+
+        <div class="card">
+
+            <h2>Methodology</h2>
+
+            <p>Details on how we measured downtown activity and recovery using mobile phone data.</p>
+
+        </div>
+
+    </div>
+
+</main>
