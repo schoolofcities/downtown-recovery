61939df91487c554322d193f474d6832e9c24c47
Jeff Allen <jeffallen89@gmail.com>
2023-10-23 19:08:29 -0400

    blog setup

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -41,8 +41,16 @@
 			<ul class="dropdown-menu">
 				<li><a href="/charts/rankings">Rankings</a></li>
 			  	<li><a href="/charts/correlations">Correlations</a></li>
+				<li><a href="/methodology">Methodology</a></li>
 			  	<li><a href="/charts/rankings_archived">Rankings (Archived)</a></li>
 			  	<li><a href="/charts/patterns">Patterns (Archived)</a></li>
+			</ul>
+		  </li>
+		  <li class="dropdown">
+			<a href="#">Blog &darr;</a>
+			<ul class="dropdown-menu">
+				<li><a href="/death_of_downtown_policy_brief.pdf">Hellow I am a wonderful blog post</a></li>
+				<li><a href="https://www.youtube.com/watch?v=K2E1uvWqSH8&feature=youtu.be" target="_blank">I am another fantastic blog post</a></li>
 			</ul>
 		  </li>
 		  <li class="dropdown">
@@ -50,7 +58,6 @@
 			<ul class="dropdown-menu">
 				<li><a href="/death_of_downtown_policy_brief.pdf">Policy Brief</a></li>
 				<li><a href="https://www.youtube.com/watch?v=K2E1uvWqSH8&feature=youtu.be" target="_blank">Explainer Video</a></li>
-				<li><a href="/methodology">Methodology</a></li>
 			</ul>
 		  </li>
 		  <li><a href="/team">Team</a></li>
@@ -108,12 +115,6 @@
 		cursor: pointer;
 	}
 
-	#line-chart {
-		position: absolute;
-		top: 10px;
-		left: 550px;
-	}
-
 	#menu {
 		background-color: var(--brandGray90);
 		height: 25px; 
@@ -153,6 +154,7 @@
 
 	.dropdown-menu {
 		display: none;
+		background-color: var(--brandGray90);
 		border-left: solid 1px var(--brandDarkBlue);
 		border-bottom: solid 1px var(--brandDarkBlue);
 		position: absolute;
@@ -160,6 +162,7 @@
 		margin-top: 0px;
 		padding-top: 14px;
 		z-index: 20;
+		max-width: 200px;
 	}
 
 	.dropdown-menu li {
---
+++ src/routes/blog/downtown-recovery-and-transit-ridership/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+
+    import Header from "../../../lib/Header.svelte";
+    import "../../../assets/global.css";
+
+    
+
+</script>
+
+
+<Header/>
+
+<main>
+
+    <div class="text">
+
+        <h1><span class="bold">Downtown Recovery & Transit Ridership Recovery</span></h1>
+
+        <p>
+            In the aftermath of the COVID-19 pandemic, the revival of downtown areas and public transit systems has been a hot topic. As cities all over North America faced pandemic challenges, we decided to explore the connection between downtown recovery and public transit resurgence. Our journey took us across different types of public transport, including Commuter Rail, Heavy Rail, Light Rail, and Buses in cities across North America. 
+        </p>
+
+    </div>
+
+    <div class="text"></div>
+
+    <div class="text"></div>
+
+
+</main>
