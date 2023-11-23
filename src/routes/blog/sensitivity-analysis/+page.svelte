ca5f9ba059e78b5afbede0d5dacdfe1e9a3991c6
JuliaGreenberg1 <juliagreenberg@berkeley.edu>
2023-11-23 08:15:02 -0800

    changed from 'drop-down menu above' to 'below'

--- src/routes/blog/sensitivity-analysis/+page.svelte
+++ src/routes/blog/sensitivity-analysis/+page.svelte
@@ -39,7 +39,7 @@
             All of these methodologies have their shortcomings; for example, they generally do not account for the fact that some cities are polycentric and have multiple downtown areas.<a href="#footnote-4">[4]</a> Given the heterogeneity of urban regions – with tremendous variety in their economic context, built environment, and geographic scale – there also may not be a standard definition of downtown that works well for multiple cities.
         </p>
         <p>
-            Because it is so challenging to come up with a single definition of downtown, we created multiple downtown polygons for each city. Use the drop-down menu above to select a city and compare the “recovery rates” of the different downtown boundaries (see our <a href='https://downtownrecovery.com/methodology' target='_blank'>methodology page</a> to learn more about how these rates were calculated).
+            Because it is so challenging to come up with a single definition of downtown, we created multiple downtown polygons for each city. Use the drop-down menu below to select a city and compare the “recovery rates” of the different downtown boundaries (see our <a href='https://downtownrecovery.com/methodology' target='_blank'>methodology page</a> to learn more about how these rates were calculated).
         </p>
     
     </div>
