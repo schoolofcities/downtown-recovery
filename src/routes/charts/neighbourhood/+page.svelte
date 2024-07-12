f18d7b9573ce1f457e253ebfd791bba448f94e56
scott christian mccallum <scottcmccallum@gmail.com>
2024-07-12 15:45:36 -0400

    add content and 3d toggle

--- src/routes/charts/neighbourhood/+page.svelte
+++ src/routes/charts/neighbourhood/+page.svelte
@@ -1,26 +1,41 @@
 <script>
+	import Header from "../../../lib/Header.svelte";
+	import "../../../assets/global.css";
 	import { onMount } from "svelte";
     import * as maplibregl from "maplibre-gl";
 	import * as pmtiles from "pmtiles";
 	import GeoJSON from "../../../assets/high-point-geohashes-grid.geo.json";
 	import "maplibre-gl/dist/maplibre-gl.css";
+	import { tweened } from "svelte/motion";
+	import { cubicOut } from 'svelte/easing';
+	import { writable } from "svelte/store";
 
 	let map;
-
 	let scale = new maplibregl.ScaleControl({
         maxWidth: 100,
         unit: "metric",
     });
 
+	const tweenedValue = tweened(0, {
+      duration: 800,
+      easing: cubicOut,
+    });
+
+	const tweenedStore = writable(0);
+	tweenedValue.subscribe(value => tweenedStore.set(value));
+
+	let isOn;
+  
+    function toggle() {
+      isOn = !isOn;
+      tweenedValue.set(isOn ? 1 : 0);
+    }
 
 	// Normalized stops values
-	const stopValues = GeoJSON.features.map(feature => feature.properties.stops);
-	const maxStop = Math.max(...stopValues);
-
 	GeoJSON.features.forEach(feature => {
 		const stops = feature.properties.stops;
-		const normalizedStops = stops/maxStop ;
-		feature.properties.normalizedStops = normalizedStops;
+		const dailyStops = (20 * stops) / 365 ;
+		feature.properties.dailyStops = dailyStops;
 	});
 
 	// const maxBounds = [
@@ -36,13 +51,13 @@
 		map = new maplibregl.Map({
 			container: "map",
 			style: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json", // Carto dark style URL
-            center: [-79.997, 35.9615],
-            zoom: 8.5,
+            center: [-79.8526, 36.1145],
+            zoom: 8.544,
             pitch: 60,
             // maxPitch: 85,
             // maxZoom: 30,
             // minZoom: 10,
-            bearing: 25,
+            bearing: -55.519,
             projection: "globe",
             scrollZoom: true,
             // maxBounds: maxBounds,
@@ -50,6 +65,16 @@
             antialias: true,
 		});
 
+
+		// Log initial camera parameters
+		// logCurrentCameraParameters();
+
+		// camera parameters
+		// map.on('moveend', () => {
+		// logCurrentCameraParameters();
+		// });
+
+		// Add controls
 		map.addControl(scale, "bottom-left");
 		map.addControl(new maplibregl.NavigationControl(), "top-left");
 
@@ -60,31 +85,116 @@
 			data: GeoJSON,
 			});
 
-			// Example with colour gradient and minimum threshold
+			// Data
 			map.addLayer({
-			id: "base-layer",
+			id: "data-layer",
 			type: "fill-extrusion",
 			source: "GeoJSON-Grid",
 			paint: {
-          		"fill-extrusion-color": ["interpolate", ["linear"], ["get", "normalizedStops"], 0, "rgba(211, 169, 40, 1)", 0.6, "rgba(211, 84, 40, 1)"],
+          		"fill-extrusion-color": [
+					'step',
+					['get', 'dailyStops'],
+						"#D3A928",
+						500,
+						"#D38D28",
+						2500,
+						"#D37028",
+						5000,
+						"#D35428",
+				],
 				"fill-extrusion-opacity": 1,
-          		"fill-extrusion-height": ["*", ["get", "normalizedStops"], 50000],
+          		"fill-extrusion-height": ["*", ["get", "dailyStops"], $tweenedStore],
 				"fill-extrusion-base": 0,
 			},
+			filter: ['>', ['get', 'dailyStops'], -1] // Threshold filter
+
 		});
-		
 	});
 
 });
 
+// Log current camera parameters
+// function logCurrentCameraParameters() {
+//     const center = map.getCenter();
+//     const zoom = map.getZoom();
+//     const bearing = map.getBearing();
+//     const pitch = map.getPitch();
+
+//     console.log('Current Camera Parameters:');
+//     console.log('Center:', center);
+//     console.log('Zoom:', zoom);
+//     console.log('Bearing:', bearing);
+//     console.log('Pitch:', pitch);
+//   }
+
+  $: if (map) {
+        const layer = map.getLayer('data-layer');
+        if (layer) {
+            map.setPaintProperty('data-layer', 'fill-extrusion-height', ["*", ["get", "dailyStops"], $tweenedStore]);
+        }
+    }
+
 </script>
 
-<div id="map"/>
+<Header/>
+
+
+<main>
+	<div class="text">
+		<h1>
+			Lorem Ipsum
+		</h1>
+		<p>
+			<i>Updated </i>
+		</p>
+		<p>
+			By <a href="">Author 1</a>, <a href="">Author 2</a>, <a href="">Author 3</a>
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ullamcorper lectus vitae ex posuere vestibulum. Curabitur dapibus est eget sagittis faucibus. Praesent id urna velit. Sed ullamcorper urna libero, interdum congue lorem venenatis a. Aenean auctor nunc hendrerit nunc pellentesque, nec congue mauris vehicula. Proin sit amet pellentesque leo, vitae scelerisque velit. Nunc congue pulvinar felis id mollis. Praesent mattis libero ex, in venenatis orci pharetra et. Praesent ultrices gravida rhoncus.
+		</p>
+		<p>
+			Praesent varius rhoncus libero eu gravida. Suspendisse vestibulum tellus a pharetra lobortis. Phasellus pulvinar nunc et ligula blandit, a suscipit tortor blandit. Ut eget molestie mi. Morbi non dictum erat. Mauris vehicula tempor nulla, porta dignissim libero ullamcorper a. Aenean vestibulum congue turpis, non consequat tellus egestas ut. Fusce fermentum vulputate dui, lacinia blandit ipsum semper et. Nunc et sollicitudin erat, sit amet egestas lectus. Fusce vulputate ex convallis sollicitudin faucibus.
+		</p>
+	<h5>High Point Visits Per Day</h5>
+	<div id="map"/>
+
+	<button on:click={toggle}>{$tweenedValue}</button>
+
+	<div id="legend">
+		<svg width="300" height="40" xmlns="http://www.w3.org/2000/svg">
+			<rect x="0" y="0" width="75" height="15" fill="#D3A928"/>
+			<rect x="75" y="0" width="75" height="15" fill="#D38D28"/>
+			<rect x="150" y="0" width="75" height="15" fill="#D37028"/>
+			<rect x="225" y="0" width="75" height="15" fill="#D35428"/>
+			<text class="legend-label" x="75" y="32" text-anchor="middle" >500</text>
+			<text class="legend-label" x="150" y="32" text-anchor="middle">2,500</text>
+			<text class="legend-label" x="225" y="32" text-anchor="middle">5,000</text>
+		</svg>
+	</div>
+	<div>
+		<h5>Lorem Ipsum</h5>	
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ullamcorper lectus vitae ex posuere vestibulum. Curabitur dapibus est eget sagittis faucibus. Praesent id urna velit. Sed ullamcorper urna libero, interdum congue lorem venenatis a. Aenean auctor nunc hendrerit nunc pellentesque, nec congue mauris vehicula. Proin sit amet pellentesque leo, vitae scelerisque velit. Nunc congue pulvinar felis id mollis. Praesent mattis libero ex, in venenatis orci pharetra et. Praesent ultrices gravida rhoncus.
+		</p>
+	</div>
+</main>
 
 <style>
     #map {
-        position: fixed;
-        width: 100%;
-        height: 100%;
+		padding-left: 10px;
+		padding-right: 10px;
+		margin: 0 auto;
+        max-width: 760px;
+        height: 500px;
     }
+
+	#legend {
+		margin: 20px auto;
+		width: 300px;
+	}
+
+	.legend-label {
+		fill: white;
+	}
   </style>
