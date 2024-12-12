9a9352b2a251c05f84c08d5fc890bc89ebb9b030
Jeff Allen <jeffallen89@gmail.com>
2024-12-12 14:55:45 -0500

    add canada trends to header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -39,6 +39,7 @@
 		<li class="dropdown">
 		<a href="#">Charts &darr;</a>
 		<ul class="dropdown-menu">
+			<li><a href="/charts/canada_trends">Trends (Canada update)</a></li>
 			<li><a href="/charts/trends">Trends</a></li>
 			<li><a href="/charts/rankings">Rankings</a></li>
 			<li><a href="/charts/correlations">Correlations</a></li>
