ac254fd5472b29a599a3019ad16d6c4b6cd29112
Jeff Allen <jeffallen89@gmail.com>
2025-11-25 12:45:02 -0500

    doom loop blog

---
+++ src/routes/blog/doom-loop/+page.svelte
@@ -0,0 +1,133 @@
+<script>
+
+    import Header from "../../../lib/Header.svelte";
+    import "../../../assets/global.css";
+
+    import RecoveryMaps from "./lib/RecoveryMaps.svelte";
+
+    import mapToronto from "./data/Q_tor.png";
+    import mapVancouver from "./data/Q_van.png";
+    import mapNewYorkSouth from "./data/Q_nyc_south.png";
+    import mapNewYorkNorth from "./data/Q_nyc_north.png";
+    import mapSanFrancisco from "./data/Q_sf.png";
+
+    import doomLoopGraphic from "./data/doom-loop-schematic.png";
+
+</script>
+
+
+<Header/>
+
+<main>
+
+    <div class="text">
+
+        <h3>Doom Loop</h3>
+
+        <p>By Author Name, Author Name, etc. | November, 2025</p> 
+
+        <!-- <a href="https://schoolofcities.utoronto.ca/people/byeonghwa-jeong/">Byeonghwa Jeong</a>, <a href="https://schoolofcities.utoronto.ca/people/karen-chapple/">Karen Chapple</a> -->
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+
+        <div style="max-width:600px; width:100%; margin:0 auto;">
+            <img
+                src="{doomLoopGraphic}"
+                alt="Descriptive alt text"
+                loading="lazy"
+                style="
+                display:block;         
+                width:100%;          
+                max-width:100%;    
+                object-fit:contain; 
+                image-rendering: -webkit-optimize-contrast; 
+                "
+            />
+        </div>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+
+        <RecoveryMaps
+            title="Downtown San Francisco"
+            mapImage={mapSanFrancisco}
+        />
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+
+        <RecoveryMaps
+            title="New York Midtown"
+            mapImage={mapNewYorkNorth}
+        />
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+
+        <RecoveryMaps
+            title="New York Financial District"
+            mapImage={mapNewYorkSouth}
+        />
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+
+        <RecoveryMaps
+            title="Downtown Toronto"
+            mapImage={mapToronto}
+        />
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        
+        <RecoveryMaps
+            title="Downtown Vancouver"
+            mapImage={mapVancouver}
+        />
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        
+    </div>
+
+
+</main>
+
+
+<style>
+
+    .mapImg {
+        max-width: 600px;
+        margin: 0 auto; /* Center the container */
+    }
+    img {
+		border: solid 1px var(--brandDarkBlue);
+		min-width: 350px;
+		max-width: 100%; /* Set maximum width to 100% of the container */
+		height: auto; /* Maintain aspect ratio */
+		display: block; /* Remove default image spacing */
+		margin: 0 auto; /* Center the image within the container */
+	}
+
+    .text {
+        border: 0px;
+    }
+
+
+    h4 {
+        text-decoration: underline;
+    }
+
+    p {
+        line-height: 25px;
+    }
+
+</style>
---
+++ src/routes/blog/doom-loop/lib/RecoveryMaps.svelte
@@ -0,0 +1,131 @@
+<script>
+
+	import mapRecovery from "../data/map-recovery.png";
+
+	let legendColours = ['#dc4633', '#e27e71', '#e9c1bb', '#eeeeee', '#c2e1ec', '#9bd5eb', '#6fc7ea'];
+	let legendText = ["50%","70%","90%", "110%", "130%", "150%"]
+
+	export let title = "";
+	export let mapImage = "";
+
+	let mapImagePath = "." + mapImage;
+
+</script>
+
+
+
+<div class="legendContainer">
+	<h2>{title}</h2>
+	<p>Recovery Rate % (2023 versus 2019)</p>
+	<svg class="legend" width="370" height="37" xmlns="http://www.w3.org/2000/svg">
+		{#each legendColours as colour, index}
+			<rect
+				x={index * 50}
+				y="0"
+				width="50"
+				height="15"
+				fill={colour}
+				stroke="#191919"
+				stroke-width="1"
+			/>
+		{/each}
+		{#each legendText as text, index}
+			<text
+				x={50 + index * 50} 
+				y="30"
+				fill="#ffffff"
+				font-size="14"
+				text-anchor="middle"
+				font-family="Roboto"
+				opacity="0.7"
+				>
+				{text}
+			</text>
+		{/each}
+
+		<!-- <defs>
+			<pattern id="diagonalHatch" patternUnits="userSpaceOnUse" width="4" height="4" patternTransform="rotate(-45)">
+				<line x1="0" y1="0" x2="0" y2="4" style="stroke:#1E3765;stroke-width:2" />
+			</pattern>
+		</defs>		
+		<rect
+			x="0"
+			y="45"
+			width="25"
+			height="15"
+			fill="#e27e71"
+			stroke="#191919"
+			stroke-width="1"
+		/>
+		<rect
+			x="25"
+			y="45"
+			width="25"
+			height="15"
+			fill="#9bd5eb"
+			stroke="#191919"
+			stroke-width="1"
+		/>
+		<rect
+			x="0"
+			y="45"
+			width="50"
+			height="15"
+			fill="url(#diagonalHatch)"
+			stroke="#191919"
+			stroke-width="0"
+		/>
+		<text
+			x="55" 
+			y="58"
+			fill="#ffffff"
+			font-size="14"
+			text-anchor="start"
+			font-family="Roboto"
+			opacity="0.7"
+			>
+			Building with low sample, treat result with caution
+		</text> -->
+	</svg>
+</div>
+
+<div class="mapImg">
+
+	<img src={mapImage} alt="Map visualizing buildings based on their footfall activity in 2023 relative to 2019 (pre-covid). Buildings shaded blue indicate recovery of greater than 100%, buildings shaded red are below 100%.">
+
+</div>
+
+
+
+  
+
+<style>
+
+	.mapImg {
+		max-width: 600px;
+		margin: 0 auto; /* Center the container */
+	}
+
+	img {
+		border: solid 1px var(--brandDarkBlue);
+		min-width: 350px;
+		max-width: 100%; /* Set maximum width to 100% of the container */
+		height: auto; /* Maintain aspect ratio */
+		display: block; /* Remove default image spacing */
+		margin: 0 auto; /* Center the image within the container */
+	}
+
+	.legendContainer {
+		padding-top: 10px;
+		/* background-color: red; */
+		border-top: solid 1px var(--brandDarkBlue);
+		max-width: 600px;
+		margin: 0 auto;
+		padding-bottom: 10px;
+	}
+
+	.legend {
+		margin: 0 auto;
+	}
+
+</style>
